c5c1688f6fe6f5e5cdcae3c8f8bd7a50fbeb7bc1
DDF <1711356221@qq.com>
2022-08-24 11:31:27 +0800

    1.19.21.01 arm64 support

--- src/Loader.c
+++ src/Loader.c
@@ -30,6 +30,11 @@ G_DEFINE_TYPE_EXTENDED(LoaderInvocationListener,
     #define READ_ASSET_FILE_ADDRESS       (minecraftpeBaseAddr + 0x490044C)
     #define RESOURCE_LOCATION_ADDRESS     (gum_module_find_export_by_name("libminecraftpe.so", "_ZN16ResourceLocationC2ERKN4Core4PathE"))
 #endif
+#ifdef V1_19_21_01
+    #define RESOURCE_PACK_MANAGER_ADDRESS (minecraftpeBaseAddr + 0x56255DC)
+    #define READ_ASSET_FILE_ADDRESS       (minecraftpeBaseAddr + 0x490044C)
+    #define RESOURCE_LOCATION_ADDRESS     (gum_module_find_export_by_name("libminecraftpe.so", "_ZN16ResourceLocationC2ERKN4Core4PathE"))
+#endif
 
 typedef enum _HookId HookId;
 enum _HookId {
