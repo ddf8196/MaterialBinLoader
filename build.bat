558f0a078cbe30b5027af5a87e1bbbbf1ae3176f
DDF <1711356221@qq.com>
2022-11-05 00:04:12 +0800

    1.19.40.02 arm64 support

--- build.bat
+++ build.bat
@@ -3,3 +3,5 @@ mkdir build
 clang.exe --target=aarch64-linux-android21 -shared -s -O2 -fPIC -DV1_19_20_02 -Wno-void-pointer-to-enum-cast -DANDROID -ffunction-sections -fdata-sections ./src/*.c -o ./build/libmaterialbinloader-1.19.20.02-arm64.so -I./frida/arm64/include -L./frida/arm64/lib -lfrida-gum -llog -ldl -lm -pthread -Wl,--gc-sections,-z,noexecstack,-z,relro,-z,now
 ::1.19.21.01 arm64-v8a
 clang.exe --target=aarch64-linux-android21 -shared -s -O2 -fPIC -DV1_19_21_01 -Wno-void-pointer-to-enum-cast -DANDROID -ffunction-sections -fdata-sections ./src/*.c -o ./build/libmaterialbinloader-1.19.21.01-arm64.so -I./frida/arm64/include -L./frida/arm64/lib -lfrida-gum -llog -ldl -lm -pthread -Wl,--gc-sections,-z,noexecstack,-z,relro,-z,now
+::1.19.40.02 arm64-v8a
+clang.exe --target=aarch64-linux-android21 -shared -s -O2 -fPIC -DV1_19_40_02 -Wno-void-pointer-to-enum-cast -DANDROID -ffunction-sections -fdata-sections ./src/*.c -o ./build/libmaterialbinloader-1.19.40.02-arm64.so -I./frida/arm64/include -L./frida/arm64/lib -lfrida-gum -llog -ldl -lm -pthread -Wl,--gc-sections,-z,noexecstack,-z,relro,-z,now
--- src/Loader.c
+++ src/Loader.c
@@ -35,6 +35,11 @@ G_DEFINE_TYPE_EXTENDED(LoaderInvocationListener,
     #define READ_ASSET_FILE_ADDRESS       (minecraftpeBaseAddr + 0x490044C)
     #define RESOURCE_LOCATION_ADDRESS     (gum_module_find_export_by_name("libminecraftpe.so", "_ZN16ResourceLocationC2ERKN4Core4PathE"))
 #endif
+#ifdef V1_19_40_02
+    #define RESOURCE_PACK_MANAGER_ADDRESS (minecraftpeBaseAddr + 0x6D5EBF0)
+    #define READ_ASSET_FILE_ADDRESS       (minecraftpeBaseAddr + 0x4C6B63C)
+    #define RESOURCE_LOCATION_ADDRESS     (gum_module_find_export_by_name("libminecraftpe.so", "_ZN16ResourceLocationC2ERKN4Core4PathE"))
+#endif
 
 typedef enum _HookId HookId;
 enum _HookId {
