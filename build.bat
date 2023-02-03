33035fe6b81afb42e66c7dd44cab501268c7475a
DDF <1711356221@qq.com>
2023-02-03 17:49:38 +0800

    1.19.51.01

--- build.bat
+++ build.bat
@@ -1,7 +1,12 @@
+set CLANG_OPTIONS=--target=aarch64-linux-android21 -shared -s -O2 -fPIC -Wno-void-pointer-to-enum-cast -DANDROID -ffunction-sections -fdata-sections -I./frida/arm64/include -L./frida/arm64/lib -lfrida-gum -llog -ldl -lm -pthread -Wl,--gc-sections,-z,noexecstack,-z,relro,-z,now
+
 mkdir build
 ::1.19.20.02 arm64-v8a
-clang.exe --target=aarch64-linux-android21 -shared -s -O2 -fPIC -DV1_19_20_02 -Wno-void-pointer-to-enum-cast -DANDROID -ffunction-sections -fdata-sections ./src/*.c -o ./build/libmaterialbinloader-1.19.20.02-arm64.so -I./frida/arm64/include -L./frida/arm64/lib -lfrida-gum -llog -ldl -lm -pthread -Wl,--gc-sections,-z,noexecstack,-z,relro,-z,now
+clang.exe %CLANG_OPTIONS% -DV1_19_20_02 ./src/*.c -o ./build/libmaterialbinloader-1.19.20.02-arm64.so
 ::1.19.21.01 arm64-v8a
-clang.exe --target=aarch64-linux-android21 -shared -s -O2 -fPIC -DV1_19_21_01 -Wno-void-pointer-to-enum-cast -DANDROID -ffunction-sections -fdata-sections ./src/*.c -o ./build/libmaterialbinloader-1.19.21.01-arm64.so -I./frida/arm64/include -L./frida/arm64/lib -lfrida-gum -llog -ldl -lm -pthread -Wl,--gc-sections,-z,noexecstack,-z,relro,-z,now
+clang.exe %CLANG_OPTIONS% -DV1_19_21_01 ./src/*.c -o ./build/libmaterialbinloader-1.19.21.01-arm64.so 
 ::1.19.40.02 arm64-v8a
-clang.exe --target=aarch64-linux-android21 -shared -s -O2 -fPIC -DV1_19_40_02 -Wno-void-pointer-to-enum-cast -DANDROID -ffunction-sections -fdata-sections ./src/*.c -o ./build/libmaterialbinloader-1.19.40.02-arm64.so -I./frida/arm64/include -L./frida/arm64/lib -lfrida-gum -llog -ldl -lm -pthread -Wl,--gc-sections,-z,noexecstack,-z,relro,-z,now
+::also compatible with 1.19.41.01 arm64-v8a
+clang.exe %CLANG_OPTIONS% -DV1_19_40_02 ./src/*.c -o ./build/libmaterialbinloader-1.19.40.02-arm64.so
+::1.19.51.01 arm64-v8a
+clang.exe %CLANG_OPTIONS% -DV1_19_51_01 ./src/*.c -o ./build/libmaterialbinloader-1.19.51.01-arm64.so
--- src/Loader.c
+++ src/Loader.c
@@ -40,6 +40,11 @@ G_DEFINE_TYPE_EXTENDED(LoaderInvocationListener,
     #define READ_ASSET_FILE_ADDRESS       (minecraftpeBaseAddr + 0x4C6B63C)
     #define RESOURCE_LOCATION_ADDRESS     (gum_module_find_export_by_name("libminecraftpe.so", "_ZN16ResourceLocationC2ERKN4Core4PathE"))
 #endif
+#ifdef V1_19_51_01
+    #define RESOURCE_PACK_MANAGER_ADDRESS (minecraftpeBaseAddr + 0x68FF3D0)
+    #define READ_ASSET_FILE_ADDRESS       (minecraftpeBaseAddr + 0x4C174FC)
+    #define RESOURCE_LOCATION_ADDRESS     (gum_module_find_export_by_name("libminecraftpe.so", "_ZN16ResourceLocationC2ERKN4Core4PathE"))
+#endif
 
 typedef enum _HookId HookId;
 enum _HookId {
