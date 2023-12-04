df0f3cf8b0e9b527b5f5c6342a6597301c7868c4
devendrn <devman2857@gmail.com>
2023-12-04 16:46:32 +0530

    Update build scripts

--- build.bat
+++ build.bat
@@ -1,68 +1,68 @@
-set CLANG_OPTIONS=--target=aarch64-linux-android21 -shared -s -O2 -fPIC -Wno-void-pointer-to-enum-cast -DANDROID -ffunction-sections -fdata-sections -I./frida/arm64/include -L./frida/arm64/lib -lfrida-gum -llog -ldl -lm -pthread -Wl,--gc-sections,-z,noexecstack,-z,relro,-z,now
+@echo off
+:: usage:
+:: .\build.bat
+:: .\bulid.bat -v 1.20.40.22 1.20.50.21
 
-mkdir build
-::1.19.20.02 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_20_02 ./src/*.c -o ./build/libmaterialbinloader-1.19.20.02-arm64.so
-::1.19.21.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_21_01 ./src/*.c -o ./build/libmaterialbinloader-1.19.21.01-arm64.so 
-::1.19.40.02 arm64-v8a
-::also compatible with 1.19.41.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_40_02 ./src/*.c -o ./build/libmaterialbinloader-1.19.40.02-arm64.so
-::1.19.51.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_51_01 ./src/*.c -o ./build/libmaterialbinloader-1.19.51.01-arm64.so
-::1.19.60.03 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_60_03 ./src/*.c -o ./build/libmaterialbinloader-1.19.60.03-arm64.so
-::1.19.62.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_62_01 ./src/*.c -o ./build/libmaterialbinloader-1.19.62.01-arm64.so
-::1.19.63.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_63_01 ./src/*.c -o ./build/libmaterialbinloader-1.19.63.01-arm64.so
-::1.19.70.22 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_70_22 ./src/*.c -o ./build/libmaterialbinloader-1.19.70.22-arm64.so
-::1.19.70.02 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_70_02 ./src/*.c -o ./build/libmaterialbinloader-1.19.70.02-arm64.so
-::1.19.71.02 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_71_02 ./src/*.c -o ./build/libmaterialbinloader-1.19.71.02-arm64.so
-::1.19.73.02 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_73_02 ./src/*.c -o ./build/libmaterialbinloader-1.19.73.02-arm64.so
-::1.19.80.02 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_80_02 ./src/*.c -o ./build/libmaterialbinloader-1.19.80.02-arm64.so
-::1.19.81.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_81_01 ./src/*.c -o ./build/libmaterialbinloader-1.19.81.01-arm64.so
-::1.19.83.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_19_83_01 ./src/*.c -o ./build/libmaterialbinloader-1.19.83.01-arm64.so
-::1.20.0.20 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_0_20 ./src/*.c -o ./build/libmaterialbinloader-1.20.0.20-arm64.so
-::1.20.0.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_0_01 ./src/*.c -o ./build/libmaterialbinloader-1.20.0.01-arm64.so
-::1.20.1.02 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_1_02 ./src/*.c -o ./build/libmaterialbinloader-1.20.1.02-arm64.so
-::1.20.10.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_10_01 ./src/*.c -o ./build/libmaterialbinloader-1.20.10.01-arm64.so
-::1.20.12.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_12_01 ./src/*.c -o ./build/libmaterialbinloader-1.20.12.01-arm64.so
-::1.20.30.20 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_30_20 ./src/*.c -o ./build/libmaterialbinloader-1.20.30.20-arm64.so
-::1.20.30.21 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_30_21 ./src/*.c -o ./build/libmaterialbinloader-1.20.30.21-arm64.so
-::1.20.15.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_15_01 ./src/*.c -o ./build/libmaterialbinloader-1.20.15.01-arm64.so
-::1.20.30.22 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_30_22 ./src/*.c -o ./build/libmaterialbinloader-1.20.30.22-arm64.so
-::1.20.30.25 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_30_25 ./src/*.c -o ./build/libmaterialbinloader-1.20.30.25-arm64.so
-::1.20.40.20 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_40_20 ./src/*.c -o ./build/libmaterialbinloader-1.20.40.20-arm64.so
-::1.20.30.02 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_30_02 ./src/*.c -o ./build/libmaterialbinloader-1.20.30.02-arm64.so
-::1.20.40.22 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_40_22 ./src/*.c -o ./build/libmaterialbinloader-1.20.40.22-arm64.so
-::1.20.31.01 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_31_01 ./src/*.c -o ./build/libmaterialbinloader-1.20.31.01-arm64.so
-::1.20.32.03 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_32_03 ./src/*.c -o ./build/libmaterialbinloader-1.20.32.03-arm64.so
-::1.20.40.24 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_40_24 ./src/*.c -o ./build/libmaterialbinloader-1.20.40.24-arm64.so
-::1.20.50.20 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_50_20 ./src/*.c -o ./build/libmaterialbinloader-1.20.50.20-arm64.so
-::1.20.50.21 arm64-v8a
-clang.exe %CLANG_OPTIONS% -DV1_20_50_21 ./src/*.c -o ./build/libmaterialbinloader-1.20.50.21-arm64.so
+set CLANG_OPTS_ARM64=--target=aarch64-linux-android21 -shared -s -O2 -fPIC -Wno-void-pointer-to-enum-cast -DANDROID -ffunction-sections -fdata-sections -I./frida/arm64/include -L./frida/arm64/lib -lfrida-gum -llog -ldl -lm -pthread -Wl,--gc-sections,-z,noexecstack,-z,relro,-z,now
+
+:: arm64-v8a
+:: 1.19.40.02 also compatible with 1.19.41.01
+set VERSIONS_ARM64=^
+    1.19.20.02^
+    1.19.21.01^
+    1.19.40.02^
+    1.19.51.01^
+    1.19.60.03^
+    1.19.62.01^
+    1.19.63.01^
+    1.19.70.22^
+    1.19.70.02^
+    1.19.71.02^
+    1.19.73.02^
+    1.19.80.02^
+    1.19.81.01^
+    1.19.83.01^
+    1.20.0.20^
+    1.20.0.01^
+    1.20.1.02^
+    1.20.10.01^
+    1.20.12.01^
+    1.20.30.20^
+    1.20.30.21^
+    1.20.15.01^
+    1.20.30.22^
+    1.20.30.25^
+    1.20.40.20^
+    1.20.30.02^
+    1.20.40.22^
+    1.20.31.01^
+    1.20.32.03^
+    1.20.40.24^
+    1.20.50.20^
+    1.20.50.21
+:: version list end
+
+set ARG_MODE=
+:loop_args
+  if "%1" == "" goto :end_args
+  if "%1" == "-v" (
+    set VERSIONS_ARM64=
+    set ARG_MODE=%1
+    goto :next_arg
+  )
+  if "%ARG_MODE%" == "-v" (
+    set VERSIONS_ARM64=%VERSIONS_ARM64% %1
+  )
+:next_arg
+    shift
+    goto :loop_args
+:end_args
+
+if not exist "build" md "build"
+
+setlocal enabledelayedexpansion
+for %%a in (%VERSIONS_ARM64%) do (
+    set DFLAG=%%a
+    echo libmaterialbinloader-%%a-arm64.so
+    clang.exe %CLANG_OPTS_ARM64% -DV!DFLAG:.=_! ./src/*.c -o ./build/libmaterialbinloader-%%a-arm64.so
+)
